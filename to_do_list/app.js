416f0eb4abeff0ca82a340445f5d19e02e76fd91
Gourav <mgourav1999@gmail.com>
2024-08-18 19:01:22 +0530

    Nothing

--- to_do_list/app.js
+++ to_do_list/app.js
@@ -4,39 +4,40 @@ const taskBox = document.querySelector('.show-tasks-list')
 let inputTask = document.querySelector('.app-wrapper input')
 
 
-addtask.addEventListener('click', () => {
-    function addTask() {
+function addTask() {
 
-        // fetching the value from the user input
-        let inputVal = inputTask.value
+    // fetching the value from the user input
+    let inputVal = inputTask.value
 
-        // when the user input and submit empty value
-        if (inputVal === '') {
-            alert('Already completed the tasks? Enjoy your day ')
-        } else {
-            // creating div with class task-wrapper
-            let taskWrapper = document.createElement('Div')
-            taskBox.append(taskWrapper)
-            taskWrapper.classList.add('task-wrapper')
+    // when the user input and submit empty value
+    if (inputVal === '') {
+        alert('Already completed the tasks? Enjoy your day ')
+    } else {
+        // creating div with class task-wrapper
+        let taskWrapper = document.createElement('Div')
+        taskBox.append(taskWrapper)
+        taskWrapper.classList.add('task-wrapper')
 
-            // appending the paragrapgh with the task input given by the user
-            let taskAdd = document.createElement('p')
-            taskAdd.textContent = inputVal
-            taskAdd.style.color = '#000000'
-            taskWrapper.append(taskAdd)
+        // appending the paragrapgh with the task input given by the user
+        let taskAdd = document.createElement('p')
+        taskAdd.textContent = inputVal
+        taskAdd.style.color = '#000000'
+        taskWrapper.append(taskAdd)
 
-            // appending the removes task button if user has completed the task
-            let removeTask = document.createElement('button')
-            removeTask.innerHTML = `&#10006;`
-            taskWrapper.append(removeTask)
-            removeTask.classList.add('remove-task')
+        // appending the removes task button if user has completed the task
+        let removeTask = document.createElement('button')
+        removeTask.innerHTML = `&#10006;`
+        taskWrapper.append(removeTask)
+        removeTask.classList.add('remove-task')
 
-            // functionality of the remove task button
-            removeTask.addEventListener('click', () =>{
-                taskWrapper.remove()
-            })
-        }
-        inputTask.value = ''
+        // functionality of the remove task button
+        removeTask.addEventListener('click', () =>{
+            taskWrapper.remove()
+        })
     }
-    addTask()
-})
+    inputTask.value = ''
+}
+
+addtask.addEventListener('click',
+    addTask
+)
